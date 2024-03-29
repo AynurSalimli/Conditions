81ace25079d4b68b7a8c9632d4125f40077368d2
AynurSalimli <aynur.selimli@ascca.edu.az>
2024-03-29 20:59:15 +0400

    conditions

---
+++ first/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Aynur!");
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+Console.WriteLine(roll1+roll2+roll3);
+
+if (roll1 > 3)
+{
+    Console.Write("It is bigger");
+}
+else{
+    Console.Write("It is not bigger");
+}
