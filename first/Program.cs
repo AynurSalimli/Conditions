7b76ab9ce43c09f6f8c2061bbd2b1d39ff636055
AynurSalimli <aynur.selimli@ascca.edu.az>
2024-03-30 23:23:07 +0400

    compare

--- first/Program.cs
+++ first/Program.cs
@@ -232,3 +232,61 @@ switch (employeeLevel)
 }
 
 Console.WriteLine($"{employeeName}, {title}");
+
+
+string sku = "01-MN-L";
+string[] product = sku.Split('-');
+
+
+string type = "";
+string color = "";
+string size = "";
+
+if (product[0] == "01")
+{
+    type = "Sweat shirt";
+}
+else if (product[0] == "02")
+{
+    type = "T-shirt";
+}
+else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}
+else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+}
+else if (product[1] == "MN")
+{
+    color = "Maroon";
+}
+else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+}
+else if (product[2] == "M")
+{
+    size = "Medium";
+}
+else if (product[2] == "L")
+{
+    size = "Large";
+}
+else
+{
+    size = "One size Fits All";
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
--- first/obj/Debug/net8.0/first.AssemblyInfo.cs
+++ first/obj/Debug/net8.0/first.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("first")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee97055eeccbef5f1480a8f37e4ca14673042ae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+545e5c345f3905d25c2e955edfeb21d7142c79de")]
 [assembly: System.Reflection.AssemblyProductAttribute("first")]
 [assembly: System.Reflection.AssemblyTitleAttribute("first")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
